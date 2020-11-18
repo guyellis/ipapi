fb6091893fb2b902528351d7084adfb4cd7f4eb4
Guy Ellis <wildfiction@gmail.com>
2020-11-18 06:37:31 -0700

    Add early error message if MAXMIND_LICENSE_KEY is missing for DB download

--- lib/download-db.ts
+++ lib/download-db.ts
@@ -4,6 +4,12 @@ import { setDbLocation } from './db/maxmind/db-helper';
 import { downloadDB } from './file-fetch-extract';
 
 const main = async (): Promise<void> => {
+  const { MAXMIND_LICENSE_KEY } = process.env;
+  if (!MAXMIND_LICENSE_KEY) {
+    console.error('You need to export MAXMIND_LICENSE_KEY');
+    process.exit();
+  }
+
   const downloadFileLocation = path.join(__dirname, downloadFolder);
   await setDbLocation(downloadFileLocation);
   await downloadDB(downloadFileLocation);
